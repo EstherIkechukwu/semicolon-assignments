b413572ad07ca897eb55cbb10cedec40a5b3a132
Esther Ikechukwu <estherik18@gmail.com>
2024-10-18 07:28:20 +0100

    assignment1

--- Java/Esther.java
+++
@@ -1,9 +0,0 @@
-public class Esther{
-	public static void main(String[]args){
-	System.out.print("Hi my name is Esther, and I write Java" );
-
-	System.out.println("\n Hi my name is Esther, and I write Java" );
-
-	System.out.printf("%s%n", "Hi my name is Esther, and I write Java" );
-}
-}
--- Java/Million.java
+++
@@ -1,9 +0,0 @@
-public class Million{
-	public static void main(String[]args){
-	
-	System.out.print("What would you do with 10 million???");
-	System.out.println("\nWhat would you do with 10 million???");
-	System.out.printf("%s%n" , "What would you do with 10 million???");
-
-	} 
-}
