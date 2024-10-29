8477b8cc0c4593be2939a5282500c09c94cc897e
Esther Ikechukwu <estherik18@gmail.com>
2024-10-29 09:31:35 +0100

    update

--- Java/assignment for 29th October/Pattern.java
+++ Java/assignment for 29th October/Pattern.java
@@ -1,41 +1,48 @@
 public class Pattern{
 	public static void main(String [] args){
 
-		System.out.println("Pattern A");
-		for(int asterisks = 1; asterisks < 7; asterisks++){
-		   for(int number = 0; number < asterisks; number++){
-			System.out.print("*");
+
+	System.out.println("Pattern A");
+	for(int asterisks = 1; asterisks <= 7; asterisks++){
+		for(int number = 1; number < asterisks; number++){
+		System.out.print("*");
 		}
 		System.out.println();
-
 	}
 
+
 	System.out.println("Pattern B");
-
-	for(int asterisks = 7; asterisks > 0; asterisks--){
-	  for(int number = 0; number < asterisks; number++){					System.out.print("*");
-
-			}
-		System.out.println();
-		
+	for(int asterisks = 7; asterisks >= 1; asterisks--){
+		for(int number = 1; number <= asterisks; number++){						System.out.print("*");
 		}
+		System.out.println();
+	}
+
+		
 
 	System.out.println("Pattern C");
-
-	for(int number = 0; number < 7; number++){
-		for(int asterisks =6; asterisks > 0; asterisks--){
+	for (int asterisks = 7; asterisks >= 1; asterisks--){
+		for (int index = 1; index <= 7 - asterisks; index++) {
 			System.out.print(" ");
-		
-			}
-		for(int index = 0; index < (asterisks-number); index++){
-
-			System.out.print("*");
-				}
-			
 		}
+		for (int number = 1; number <= asterisks; number++){
+			System.out.print("*");
+		}
+		System.out.println();
+	}
 
-
-
+	System.out.println("Pattern D");
+	for (int asterisks = 1; asterisks <= 7; asterisks++){
+		for(int next = 1; next <= 7 - asterisks ; next++){
+			System.out.print(" ");
+		}
+		for (int number = 1; number <= asterisks; number++){
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	
+	
 	}
 
 }
--- Java/assignment for 29th October/TaxCalculator.java
+++ Java/assignment for 29th October/TaxCalculator.java
@@ -1,10 +1,33 @@
 import java.util.Scanner;
 public class TaxCalculator{
-	public static void main(String [] args){
+	public static void main(String[] args){
 	Scanner input = new Scanner(System.in);
 
+	System.out.print("Enter the first citizen's name: ");
+	String citizenNameOne = input.nextLine();
 	
-	
+	System.out.print("Enter the first citizen's earning: ");
+	int citizenOneEarning = input.nextLine();
+
+	System.out.print("Enter the second citizen's name: ");
+	String citizenNameTwo = input.nextLine();
+
+	System.out.print("Enter the first citizen's earning: ");
+	String citizenTwoEarning = input.nextLine();
+
+	System.out.print("Enter the third citizen's name: ");
+	String citizenNameThree = input.nextLine();
+
+	System.out.print("Enter the first citizen's earning: ");
+	String citizenThreeEarning = input.nextLine();
+
+	firstTaxRate = 15/100;
+	secondTaxRate = 20/100;
+
+		if(citizenNameOne > 0 && citizenNameOne <= 30,000){
+
+		} 
+
 
 
 	}
