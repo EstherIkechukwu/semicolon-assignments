98d2575b55ee112f91f47c217c6599f56a903169
Esther Ikechukwu <estherik18@gmail.com>
2025-03-18 16:52:33 +0100

    mySQl

---
+++ Java/Multiples.java
@@ -0,0 +1,16 @@
+public class Multiples{
+
+
+	public static 
+
+	public static boolean isMultiple(int firstNumber, int secondNumber) {
+		if(firstNumber * firstNumber == secondNumber){
+			return true;
+		}
+		else if(secondNumber * secondNumber == firstNumber) {
+			return false;
+		}
+	}
+
+
+}
